f97f38ecadb8aa7cfa72a40c749ff93c16aa5f61
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2025-08-28 15:46:48 +0600

    functonality done

--- emergency.js
+++ emergency.js
@@ -1,30 +1,61 @@
+const historyData = [];
+
+function clearData() {
+  const transctionContainer = document.getElementById("transction-container");
+  transctionContainer.innerHTML = "";
+}
+
 function countTotalCopy() {
   let totalCount = parseInt(document.getElementById("total-count").innerText);
   totalCount += 1;
   document.getElementById("total-count").innerText = totalCount;
 }
 
-function callCalculation() {
+function callHistoryCalculation(serviceName, serviceNumber) {
   let totalCoin = parseInt(document.getElementById("total-coin").innerText);
   if (totalCoin < 20) {
-    alert("");
+    alert("Invalid");
   } else {
     totalCoin = totalCoin - 20;
     document.getElementById("total-coin").innerText = totalCoin;
 
-    const serviceName = document.getElementById("national-emergence").innerText;
-    const serviceNumber = document.getElementById(
-      "national-emergence-number"
-    ).innerText;
+    const sName = document.getElementById(serviceName).innerText;
+    const sNumber = document.getElementById(serviceNumber).innerText;
 
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
-      .toString()
-      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    const date = new Date().toLocaleTimeString();
 
-    console.log(formattedTime);
+    const data = {
+      name: sName,
+      number: sNumber,
+      cdate: date,
+    };
+
+    historyData.push(data);
+
+    const transctionContainer = document.getElementById("transction-container");
+    transctionContainer.innerHTML = "";
+
+    for (const hdata of historyData) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+                    <div class="flex justify-between items-center bg-[#FAFAFA] px-2 py-2 mt-2 rounded-xl">
+                        <div>
+                            <h2>${hdata.name}</h2>
+                            <p class="text-[#5C5C5C]">${hdata.number}</p>
+                        </div>
+                        <div>
+                            <p>${hdata.cdate}</p>
+                        </div>
+                    </div>  `;
+
+      transctionContainer.appendChild(div);
+    }
   }
+}
+
+function copyText() {
+  const phoneNumber = document.getElementById(
+    "national-emergence-number"
+  ).innerText;
+  navigator.clipboard.writeText(phoneNumber);
 }
